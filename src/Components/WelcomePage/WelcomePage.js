c14fc1b2a1831a7255373bd3965d8d7647b0802d
fannystenberg <ffannystenberg@gmail.com>
2023-06-13 11:29:01 +0200

    added lottie animation and styling to welcomepage

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Switch } from '@mui/material';
+import { Switch, Typography } from '@mui/material';
 import { styled } from 'styled-components';
+import { Player } from '@lottiefiles/react-lottie-player';
 import { Form } from './Form';
 
 export const WelcomePage = () => {
@@ -12,10 +13,33 @@ export const WelcomePage = () => {
 
   return (
     <Wrapper>
-      <Image />
+      <LottieWrapper>
+        <Player
+          style={{ height: '200px' }}
+          src="https://assets7.lottiefiles.com/packages/lf20_jif9vljs.json"
+          loop
+          autoplay
+          speed={1.0} />
+        <TextWrapper>
+          <Typography variant="body1" fontSize="18px" fontWeight="600">
+            Save your favorite locations all in one place
+          </Typography>
+          <Typography variant="body1" fontSize="16px">
+            Sign in or create an account to get started!
+          </Typography>
+        </TextWrapper>
+      </LottieWrapper>
       <FormWrapper>
-        {!checked ? <Form path="signin" title="Sign in" btnText="Sign in" /> : <Form path="signup" title="Sign up" btnText="Sign up" />}
-        <Switch checked={checked} onChange={handleToggle} />
+        {!checked ? <Form path="signin" title="Sign in" btnText="Sign in" /> : <Form path="signup" title="Create account" btnText="Sign up" />}
+        <ToggleWrapper>
+          <Typography variant="body1" fontSize="16px">
+            Sign in
+          </Typography>
+          <Switch checked={checked} onChange={handleToggle} />
+          <Typography variant="body1" fontSize="16px">
+            Create account
+          </Typography>
+        </ToggleWrapper>
       </FormWrapper>
     </Wrapper>
   );
@@ -24,7 +48,6 @@ export const WelcomePage = () => {
 export const Wrapper = styled.section`
     display: flex;
     flex-direction: column;
-    // background-color: rgb(23, 22, 22);
 
     @media (min-width: 667px) {
       display: grid;
@@ -32,20 +55,35 @@ export const Wrapper = styled.section`
     }
 `;
 
-export const Image = styled.div`
-    background-image: url('https://images.unsplash.com/photo-1619468129361-605ebea04b44?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODU0NzE5NDV8&ixlib=rb-4.0.3&q=85');
-    height: 20vh;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
+export const LottieWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-height: 20vh;
+    margin: 40px 10px 20px 10px;
 
     @media (min-width: 667px) {
-      height: 100vh;
+      max-height: 100vh;
     }
 `;
 
 export const FormWrapper = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
+`;
+
+export const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    margin: 5px 0;
+`;
+
+export const ToggleWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
     align-items: center;
 `;
